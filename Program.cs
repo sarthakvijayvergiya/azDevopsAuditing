e627141ce4c455e74bd4dc2b3f21f69c48fe61aa
Sarthak Vijayvergiya <sarthak@nibejpi.com>
2020-09-07 01:38:05 +0530

    initialize console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azDevopsAuditing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
