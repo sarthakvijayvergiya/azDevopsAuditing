63751ed347a1f65199af01f3257e7fa848fcc8fa
Sarthak Vijayvergiya <sarthak@nibejpi.com>
2020-09-12 13:20:06 +0530

    update hub endpoint

--- AzDevopsAuditingViewer/Startup.cs
+++ AzDevopsAuditingViewer/Startup.cs
@@ -1,3 +1,4 @@
+using AzDevOpsAuditingViewer.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -55,6 +56,7 @@ namespace AzDevOpsAuditingViewer
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<GridEventsHub>("/hubs/gridevents");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
